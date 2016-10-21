18dc9a6567b3fcae2fbc7f430075120a4effbf1d
ec101 <emmetclarke@gmail.com>
2016-10-21 08:47:08 +0100

    TEST COMMIT

--- app/src/main/java/com/example/roloughlin/githubproject/MainActivity.java
+++ app/src/main/java/com/example/roloughlin/githubproject/MainActivity.java
@@ -10,5 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    /*cleaning up your mess again*/
+    /*cleaning up your mess*/
 }
